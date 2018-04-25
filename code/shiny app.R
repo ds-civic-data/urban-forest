144f8754e82d37f9043ef80bc692dfe4fde33adf
Mitzi Zitler <mitzizitler@c132h099.wless.reed.edu>
2018-04-24 23:25:12 -0700

    nav bar shiny app

--- code/shiny app.R
+++ code/shiny app.R
@@ -43,40 +43,31 @@ select_fill_options <- c("total_population", "population_density", "white",
                          "renter_occupied_units", "owner_occupied_units",
                          "10_less_commute", "10_19_commute", "20_29_commute",
                          "30_39_commute", "40_59_commute", "60_89_commute",
-                         "90_more_commute", "no_commute", "Total_Pop_",
-                         "Spanish", "Russian", "Chinese", "Japanese", "Korean",
-                         "Mon_Khmer_", "Laotian", "Vietnamese", "Tagalog",
-                         "Arabic", "African")
+                         "90_more_commute", "no_commute")
 # need to rename some of these variables ^
 bounds_options <- c("black", "white", "transparent")
-#min_tree <- nrow(tree_sample)
-#max_tree <- nrow(tree_sample)
-
 
 # Define UI for application that plots 
-ui <- fluidPage(
+ui <- navbarPage(
+  "Portland: Trees & Demographics", fluid = T, collapsable = T,
+  tabPanel("Map Visualization",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("fill_opts", "Select Data Fill",
+                           choices = select_fill_options),
+               selectInput("bounds_opts", "Toggle Neighborhood Boundaries",
+                           choices = bounds_options),
+               # alpha opts could also be a slider
+               sliderInput("alpha_opts", "Select Tree Transparency",
+                           min = 0, max = 1, value = 0.01),
+               sliderInput("tree_sample", "Select Trees in Sample", 
+                           min=0, max=216751, value=10000)),
+             mainPanel(plotOutput("geom_map")))),
+  tabPanel("Data Table Output"),
+  # dataTableOutput(), 
+  tabPanel("Regression Analysis")
   
-  # Application title
-  titlePanel("Portland: Trees & Demographics"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("fill_opts", "Select Data Fill",
-                  choices = select_fill_options),
-      selectInput("bounds_opt", "Toggle Neighborhood Boundaries",
-                  choices = bounds_options),
-      # alpha opts could also be a slider
-      sliderInput("alpha_opts", "Tree Color Options",
-                  min = 0, max = 1, value = 0.01),
-      sliderInput("tree_sample", "Select Trees in Sample", 
-                  min=0, max=216751, value=10000)
-      # , dataTableOutput() -> data table for a selected neighborhood/censustract?
-    ),
-    
-      mainPanel(
-      plotOutput("geom_map"))
-  ))
+)
 
 
 server <- function(input, output) {
@@ -87,29 +78,31 @@ server <- function(input, output) {
   
   output$geom_map <- renderPlot({
     
-    ggmap(portland) + 
+    ggmap(portland) +
       geom_polygon(data = tidytract2016_spatial, 
                    aes_string(x=tidytract2016_spatial$long, 
                               y=tidytract2016_spatial$lat, 
                               group=tidytract2016_spatial$group, 
                               fill=input$fill_opts), 
                    alpha = 0.6) +
-      geom_polygon(data = neighborhoods_all, 
-                   aes(x=long, y=lat, group=group), 
-                   col = input$bounds_opt, fill = 'transparent') +
+      geom_polygon(data = neighborhoods_all, aes(x=long, y=lat, group=group), 
+                   col = input$bounds_opts, 
+                   fill = 'transparent') +
       geom_point(data = street_trees_all_filtered(),
-                 aes(x=X, y=Y), col = 'dark green',
-                 alpha = input$alpha_opts, size = 0.75) +
+                 aes(x=X, y=Y #, size=`Canopy Coverage`
+                 ), 
+                 col = 'dark green',
+                 size = 0.3,
+                 alpha = input$alpha_opts)  +
       scale_fill_gradientn(colours = heat.colors(7), na.value = 'transparent') +
       theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
       theme_bw()
-  
-     
+    
+    
   })
- 
+  
   #output$data_table <- renderTable({}) 
 }
-
 
 shinyApp(ui = ui, server = server)
 
