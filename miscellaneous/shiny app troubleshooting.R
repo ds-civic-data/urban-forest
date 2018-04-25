19a22d4e5406ef02f622b1fb200b3e6b3f1976fd
Mitzi Zitler <mitzizitler@c132h099.wless.reed.edu>
2018-04-24 23:50:58 -0700

    more troubleshooting for troubleshooting

--- miscellaneous/shiny app troubleshooting.R
+++ miscellaneous/shiny app troubleshooting.R
@@ -1,5 +1,3 @@
-## anything you load here can be seen by both ui and server
-
 library(shiny)
 library(tidyverse)
 library(readr)
@@ -17,13 +15,15 @@ lep_tidytract2016 <- read_csv('~/urban-forest/data/lep_tidytract2016.csv',
                               col_names = T)
 tidytract2016_spatial <- read_csv('~/urban-forest/data/tidytract2016_spatial.csv',
                                   col_names = T)
+tidytract2016 <- read_csv("~/urban-forest/data/tidytract2016.csv",
+                          col_names = T)
 street_trees_all <- read_csv('~/urban-forest/data/street_trees_all.csv', 
                              col_names = T)
 neighborhoods_all <- read_csv('~/urban-forest/data/neighborhoods_all.csv', 
                               col_names = T)
 portland <- get_map(location = c(lon = -122.66, lat = 45.531), zoom = 11, 
                     maptype = "terrain")
-##specify what categories we want to color with
+
 select_fill_options <- c("total_population", "population_density", "white", 
                          "black", "am_indian", "asian", "pac_isl", "other_race",
                          "two_more_races", "below_hs", "hs_degree", "some_college",
@@ -42,13 +42,10 @@ select_fill_options <- c("total_population", "population_density", "white",
                          "10_less_commute", "10_19_commute", "20_29_commute",
                          "30_39_commute", "40_59_commute", "60_89_commute",
                          "90_more_commute", "no_commute")
-# need to rename some of these variables ^
+
 bounds_options <- c("black", "white", "transparent")
-#min_tree <- nrow(tree_sample)
-#max_tree <- nrow(tree_sample)
 
 
-# Define UI for application that plots 
 ui <- navbarPage(
   "Portland: Trees & Demographics", fluid = T, collapsable = T,
     tabPanel("Map Visualization",
@@ -56,21 +53,34 @@ ui <- navbarPage(
                  sidebarPanel(
                    selectInput("fill_opts", "Select Data Fill",
                                choices = select_fill_options),
-                   selectInput("bounds_opts", "Toggle Neighborhood Boundaries",
-                               choices = bounds_options),
-                   # alpha opts could also be a slider
+                   checkboxInput("bounds_opts", "Toggle Neighborhood Boundaries",
+                               value = FALSE),
                    sliderInput("alpha_opts", "Select Tree Transparency",
-                               min = 0, max = 1, value = 0.01),
+                               min = 0, max = 1, value = 0.03),
                    sliderInput("tree_sample", "Select Trees in Sample", 
-                               min=0, max=216751, value=10000)),
+                               min=0, max=216751, value=100000)),
                  mainPanel(plotOutput("geom_map")))),
-    tabPanel("Data Table Output"),
- # dataTableOutput(), 
-    tabPanel("Regression Analysis")
+    tabPanel("Data Table Output",
+             sidebarLayout(
+               sidebarPanel(
+                 selectizeInput(
+                   "show_vars",
+                   "Columns to show:",
+                   choices = colnames(tidytract2016),
+                   multiple = TRUE,
+                   selected = c("qualifying_name", "med_family_income", 
+                                "unemployment_rate")
+                 ),
+                 actionButton("button", "Load Data"),
+                 uiOutput("category1")
+               ),
+               mainPanel(tableOutput("table")))),
+    tabPanel("Regression Analysis"),
+  tabPanel("Read Me Files")
 
   )
 
-##Server is where all of the computations happen
+
 server <- function(input, output) {
   
   street_trees_all_filtered <- reactive({
@@ -81,9 +91,6 @@ server <- function(input, output) {
     
     ggmap(portland) +
             geom_polygon(data = tidytract2016_spatial, 
-                         #because of the left_join, we have long.x and long.y
-                         #that's going to be a huge problem!!
-                         #maybe we do it without the left_join ?
                    aes_string(x=tidytract2016_spatial$long, 
                               y=tidytract2016_spatial$lat, 
                               group=tidytract2016_spatial$group, 
@@ -101,11 +108,37 @@ server <- function(input, output) {
       scale_fill_gradientn(colours = heat.colors(7), na.value = 'transparent') +
       theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
       theme_bw()
-          
-     
   })
-  
-  #output$data_table <- renderTable({}) 
+  tidytract2016.react <- eventReactive(input$button, {
+    tidytract2016[, input$show_vars]
+  })
+  observeEvent(input$button, {
+    output$category1 <- renderUI({
+      tidytract2016.sel <- tidytract2016.react()
+      selectizeInput('cat1',
+                     'Choose Tract',
+                     choices = c("All", sort(as.character(
+                       unique(tidytract2016.sel$qualifying_name)
+                     ))),
+                     selected = "All")
+    })
+    
+    df_subset <- eventReactive(input$cat1, {
+      tidytract2016.sel <- tidytract2016.react()
+      if (input$cat1 == "All") {
+        tidytract2016.sel
+      }
+      else{
+        tidytract2016.sel[tidytract2016.sel$qualifying_name == input$cat1,]
+      }
+    })
+    
+    
+    output$table <- renderTable({
+      df_subset()
+      
+    })
+  })
 }
 
 # Run the application 
