a55d28c4b27c72b3bb71dd168378059498339771
Frank Gaunt <FrankGaunt@Franks-MacBook-Air.local>
2018-04-24 17:38:36 -0700

    table now has search feature

--- code/shiny_table.R
+++ code/shiny_table.R
@@ -19,11 +19,51 @@ column_options <- c("total_population", "population_density", "area", "white", "
                     "owner_occupied_units", "10_less_commute", "10_19_commute", "20_29_commute", "30_39_commute", 
                     "40_59_commute", "60_89_commute", "90_more_commute", "no_commute")
 
-shinyApp(
-  ui = fluidPage(DTOutput('tabletract')),
-  server = function(input, output) {
-    output$tabletract = renderDT(
-      tidytract2016, options = list(lengthChange = FALSE)
-    )
-  }
+
+fluidPage(
+  
+  title = 'Select Table Rows',
+  
+  h1('ACS Data'),
+  
+  fluidRow(
+    column(6, DT::dataTableOutput('x1')),
+    column(6, plotOutput('x2', height = 500))
+  ),
+  
+  hr(),
+  
+  h1('A Server-side Table'),
+  
+  fluidRow(
+    column(9, DT::dataTableOutput('x3')),
+    column(3, verbatimTextOutput('x4'))
+  )
+  
 )
+shinyServer(function(input, output, session) {
+  
+  output$x1 = DT::renderDataTable(tidytract2016, server = FALSE)
+  
+  # highlight selected rows in the scatterplot
+  output$x2 = renderPlot({
+    s = input$x1_rows_selected
+    par(mar = c(4, 4, 1, .1))
+    plot(tidytract2016)
+    if (length(s)) points(tidytract2016[s, , drop = FALSE], pch = 19, cex = 2)
+  })
+  
+  # server-side processing
+  tidytract20162 = tidytract2016[, 1:8]
+  output$x3 = DT::renderDataTable(tidytract20162, server = TRUE)
+  
+  # print the selected indices
+  output$x4 = renderPrint({
+    s = input$x3_rows_selected
+    if (length(s)) {
+      cat('These rows were selected:\n\n')
+      cat(s, sep = ', ')
+    }
+  })
+  
+})
