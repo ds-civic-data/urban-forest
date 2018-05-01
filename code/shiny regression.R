c28a1de3596c90348da36543154b6805e0943aa7
Mitzi Zitler <mitzizitler@c132h099.wless.reed.edu>
2018-04-30 18:35:24 -0700

    data

--- code/shiny regression.R
+++ code/shiny regression.R
@@ -24,6 +24,8 @@ centroids_tt <- cbind(centroids_tt, dist)
 tidytract2016_sp_cent <- left_join(centroids_tt, tidytract2016_spatial,
                                    by = c('PID' = 'id'))
 
+write_csv(tidytract2016_sp_cent, 'tidytract2016_sp_cent.csv')
+
 m1 <- lm(population_density ~ white + black + med_family_income + dist,
          data = tidytract2016_sp_cent)
 summary(m1)
