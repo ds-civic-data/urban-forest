5c667d58e25d2f983f15136aaad915acbfd2a153
Frank Gaunt <FrankGaunt@Franks-MacBook-Air.local>
2018-05-06 03:39:53 -0700

    variable choice changes, corrected ggplot

--- code/shiny_table_tests.R
+++ code/shiny_table_tests.R
@@ -1,19 +1,19 @@
 shinyApp(
   ui = fluidPage(
     selectInput("variable1", "Variable 1:",
-                c("Median Family Income" = "med_family_income",
-                  "Unemployment Rate" = "unemployment_rate")),
+                colnames(tidytract2016),
+                selected = "med_family_income"),
     selectInput("variable2", "Variable 2:",
-                c("Median Family Income" = "med_family_income",
-                  "Unemployment Rate" = "unemployment_rate")),
-   fluidRow(column(6, tableOutput("data")),
-            column(6, plotOutput("outplot")))
+                colnames(tidytract2016),
+                selected = "owner_occupied_units"),
+    fluidRow(column(6, tableOutput("data")),
+             column(6, plotOutput("outplot")))
   ),
   server = function(input, output) {
     output$data <- renderTable({
       tidytract2016[, c("qualifying_name", input$variable1, input$variable2), drop = FALSE]
     }, rownames = F)
     output$outplot <- renderPlot({
-      ggplot(tidytract2016, aes_string(x = input$variable1)) +
-        geom_point(aes_string(y = input$variable1))  })
-    })
+      ggplot(tidytract2016, aes_string(x = input$variable1, y = input$variable2)) +
+        geom_point()  })
+  })
