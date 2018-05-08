5b7dd477d8358ded74ca257eb9e9493db13b5cb4
Frank Gaunt <FrankGaunt@Franks-MacBook-Air.local>
2018-05-08 15:50:18 -0700

    transfer new variable to tidytract_spatial

--- code/Making TidyTract2016 Spatial.R
+++ code/Making TidyTract2016 Spatial.R
@@ -14,4 +14,4 @@ tidytract2106 <- read_csv('~/urban-forest/data/tidytract2016.csv', col_names = T
 tidytract2016_spatial <- full_join(census_spatial_all, tidytract2106, 
                                    by = c('GEOID' = 'fips'))
 
-write_csv(tidytract2016_spatial, 'tidytract2016_spatial.csv')
+write_csv(tidytract2016_spatial, '~/urban-forest/data/tidytract2016_spatial.csv')
