2365a361cda0135c29fa11659938fb7c87421fe6
Frank Gaunt <FrankGaunt@Franks-MacBook-Air.local>
2018-05-08 18:53:11 -0700

    made newtidytract2016 includes canopy coverage

--- miscellaneous/shiny_table_tests.R
+++ miscellaneous/shiny_table_tests.R
@@ -1,22 +1,29 @@
+newtidytract2016 <- left_join(tidytract2016, tractCoverage, by = c("fips" = "GEOID"))
+newtidytract2016 <- newtidytract2016 %>%
+  mutate("denominator" = 1 - percent_water,
+         "% Canopy Coverage" = cover/denominator)
+newtidytract2016 <- newtidytract2016[,c(1:56, 60)]
+write_csv(newtidytract2016, "~/urban-forest/data/newtidytract2016.csv")
+
 shinyApp(
   ui = fluidPage(
     selectInput("variable1", "Variable 1:",
-                colnames(tidytract2016[, c(3:5, 13:19, 27:56)]),
+                colnames(newtidytract2016[, c(3:5, 13:19, 27:57)]),
                 selected = "% Population: Doctorate Degree"),
     selectInput("variable2", "Variable 2:",
-                colnames(tidytract2016[, c(3:5, 13:19, 27:56)]),
-                selected = "Median Family Income"),
+                colnames(newtidytract2016[, c(3:5, 13:19, 27:57)]),
+                selected = "% Canopy Coverage"),
     fluidRow(column(7, DT::dataTableOutput("mytable")),
              column(5, plotOutput("outplot")))
   ),
   server = function(input, output) {
     output$mytable <- DT::renderDataTable({
-      tidytract2016[, c("Census Tract", input$variable1, input$variable2), drop = FALSE]
+      newtidytract2016[, c("Census Tract", input$variable1, input$variable2), drop = FALSE]
     }, rownames = F)
     output$outplot <- renderPlot({
     s = input$mytable_rows_selected
-    plot(tidytract2016[, c(input$variable1, input$variable2), drop = FALSE])
-    if (length(s)) points(tidytract2016[s, c(input$variable1, input$variable2), drop = FALSE], pch = 19, cex = 2)
+    plot(newtidytract2016[, c(input$variable1, input$variable2), drop = FALSE])
+    if (length(s)) points(newtidytract2016[s, c(input$variable1, input$variable2), drop = FALSE], pch = 19, cex = 2)
   })
 })
 
