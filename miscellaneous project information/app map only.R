97e343a92fca493814de825cfb8e1d0c818466c1
Mitzi L Zitler <mitzitler@rstudio-dev.reed.edu>
2018-05-10 18:21:25 -0700

    jklhsvlef

--- miscellaneous project information/app map only.R
+++ miscellaneous project information/app map only.R
@@ -1,5 +1,3 @@
-## packages
-
 library(shiny)
 library(tidyverse)
 library(readr)
@@ -39,31 +37,31 @@ newtidytract2016 <- read_csv("~/urban-forest/data/newtidytract2016.csv") %>%
 ##specify what categories we want to color with
 Population <- list("`Total Population`", "`Population Density (Per Sq. Mile)`")
 `Race & Ethnicity` <- list("`% Total Population: White Alone`", 
-                       "`% Total Population: Black or African American Alone`", 
-                       "`% Total Population: American Indian and Alaska Native Alone`", 
-                       "`% Total Population: Asian Alone`", "`% Total Population: Native Hawaiian and Other Pacific Islander Alone`", 
-                       "`% Total Population: Some Other Race Alone`", "`% Total Population: Two or More Races`")
+                           "`% Total Population: Black or African American Alone`", 
+                           "`% Total Population: American Indian and Alaska Native Alone`", 
+                           "`% Total Population: Asian Alone`", "`% Total Population: Native Hawaiian and Other Pacific Islander Alone`", 
+                           "`% Total Population: Some Other Race Alone`", "`% Total Population: Two or More Races`")
 `Education & Unemployment` <- list("`% Population: Less than High School`", "`% Population: High School Graduate`", 
-               "`% Population: Some College`", "`% Population: Bachelor's Degree`", 
-               "`% Population: Master's Degree`", "`% Population: Professional School`", 
-               "`% Population: Doctorate Degree`", "`% Civilian Population in Labor Force 16 Years and Over: Unemployed`")
+                                   "`% Population: Some College`", "`% Population: Bachelor's Degree`", 
+                                   "`% Population: Master's Degree`", "`% Population: Professional School`", 
+                                   "`% Population: Doctorate Degree`", "`% Civilian Population in Labor Force 16 Years and Over: Unemployed`")
 `Income & Equity` <- list("`Median Family Income`", "`Per Capita Income (In 2016 Inflation Adjusted Dollars)`", 
-                       "`% Households: Less than $40,000`", "`% Households: $40,000 to $75,000`",
-                       "`% Households: $75,000 to $125,000`", "`% Households: $125,000 and Over`", 
-                       "`Gini Index`", "`% Households: No Earnings`", "`% Households: with Wage or Salary Income`", 
-                       "`% Households: with Self-Employment Income`", "`% Households: with Interest, Dividends, or Net Rental Income`", 
-                       "`% Households: with Social Security Income`", "`% Households: with Supplemental Security Income (Ssi)`", 
-                       "`% Households: with Public Assistance Income`", "`% Households: with Retirement Income`", 
-                       "`% Households: with Other Types of Income`")
+                          "`% Households: Less than $40,000`", "`% Households: $40,000 to $75,000`",
+                          "`% Households: $75,000 to $125,000`", "`% Households: $125,000 and Over`", 
+                          "`Gini Index`", "`% Households: No Earnings`", "`% Households: with Wage or Salary Income`", 
+                          "`% Households: with Self-Employment Income`", "`% Households: with Interest, Dividends, or Net Rental Income`", 
+                          "`% Households: with Social Security Income`", "`% Households: with Supplemental Security Income (Ssi)`", 
+                          "`% Households: with Public Assistance Income`", "`% Households: with Retirement Income`", 
+                          "`% Households: with Other Types of Income`")
 Housing <- list("`% Occupied Housing Units: Owner Occupied`", 
-             "`Median Gross Rent`")
+                "`Median Gross Rent`")
 `Commute Time` <- list("`% Workers with Less than 20 Minute Commute`",
-                    "`% Workers with 20 to 40 Minute Commute`", "`% Workers with Over 40 Minute Commute`",
-                    "`% Workers with No Commute`")
+                       "`% Workers with 20 to 40 Minute Commute`", "`% Workers with Over 40 Minute Commute`",
+                       "`% Workers with No Commute`")
 sfo <- list(Population, `Race & Ethnicity`, `Education & Unemployment`,
-         `Income & Equity`, Housing, `Commute Time`)
+            `Income & Equity`, Housing, `Commute Time`)
 select_fill_options <- c("`Total Population`", "`Population Density (Per Sq. Mile)`",
-                       #  "`Area (Land, in Sq. Miles)`", 
+                         #  "`Area (Land, in Sq. Miles)`", 
                          "`% Total Population: White Alone`", 
                          "`% Total Population: Black or African American Alone`", 
                          "`% Total Population: American Indian and Alaska Native Alone`", 
@@ -84,7 +82,7 @@ select_fill_options <- c("`Total Population`", "`Population Density (Per Sq. Mil
                          "`Median Gross Rent`", "`% Workers with Less than 20 Minute Commute`",
                          "`% Workers with 20 to 40 Minute Commute`", "`% Workers with Over 40 Minute Commute`",
                          "`% Workers with No Commute`")
- 
+
 # for boundary toggle
 bounds_options <- c("black", "white", "transparent")
 
@@ -105,43 +103,7 @@ ui <- navbarPage(
                              value = FALSE),
                sliderInput("tree_sample", "Select Trees in Sample", 
                            min=0, max=100000, value=50000)),
-             mainPanel(plotOutput("geom_map")))),
-  # second panel: data table and base r scatterplot
-  tabPanel("Data Table Output",
-           selectInput("variable1", "Variable 1:",
-                       list(colnames(newtidytract2016[,57]),
-                            "Population & Land" = c(colnames(newtidytract2016[,3:5])),
-                            "Race & Ethnicity" = c(colnames(newtidytract2016[,13:19])),
-                            "Education" = c(colnames(newtidytract2016[,27:33])),
-                            "Main Income & Employment Statistics" = c(colnames(newtidytract2016[,c(35,45,34,46)])),
-                            "Household Income Sources" = c(colnames(newtidytract2016[,37:44])),
-                            "Household Income Brackets" = c(colnames(newtidytract2016[,49:52])),
-                            "Housing" = c(colnames(newtidytract2016[,47:48])),
-                            "Commute Time" = c(colnames(newtidytract2016[,53:56]))),
-                       selected = "% Canopy Coverage"),
-           selectInput("variable2", "Variable 2:",
-                       list(colnames(newtidytract2016[,57]),
-                            "Population & Land" = c(colnames(newtidytract2016[,3:5])),
-                            "Race & Ethnicity" = c(colnames(newtidytract2016[,13:19])),
-                            "Education" = c(colnames(newtidytract2016[,27:33])),
-                            "Main Income & Employment Statistics" = c(colnames(newtidytract2016[,c(35,45,34,46)])),
-                            "Household Income Sources" = c(colnames(newtidytract2016[,37:44])),
-                            "Household Income Brackets" = c(colnames(newtidytract2016[,49:52])),
-                            "Housing" = c(colnames(newtidytract2016[,47:48])),
-                            "Commute Time" = c(colnames(newtidytract2016[,53:56]))),
-                       selected = "% Canopy Coverage"),
-           fluidRow(column(7, DT::dataTableOutput("mytable")),
-                    column(5, plotOutput("outplot"))),
-           # output: interactive table
-             mainPanel(tableOutput("table"))),
-  # third panel: our spatial regression with diagnostics
-  tabPanel("Regression Analysis",
-           includeMarkdown('~/urban-forest/miscellaneous project information/Regression File.Rmd')),
-  
-  # fourth panel: our about page, including downloadable links to some data
-  tabPanel("About",
-           shiny::includeMarkdown('~/urban-forest/miscellaneous project information/About File.Rmd')
-           , width = 100)
+             mainPanel(plotOutput("geom_map"))))
   
 )
 
@@ -178,17 +140,6 @@ server <- function(input, output) {
     
   })
   
-  # output of table, dependent on drop down choices for variables 1 and 2
-  output$mytable <- DT::renderDataTable({
-    newtidytract2016[, c("Census Tract", input$variable1, input$variable2), drop = FALSE]
-  }, rownames = F)
-  # output of scatterplot, x = variable1, y = variable2, s records selected rows and adds points with 'if' line
-  output$outplot <- renderPlot({
-    s = input$mytable_rows_selected
-    plot(newtidytract2016[, c(input$variable1, input$variable2), drop = FALSE])
-    if (length(s)) points(newtidytract2016[s, c(input$variable1, input$variable2), drop = FALSE], pch = 19, cex = 2)
-  })
-      
 }
 
 shinyApp(ui = ui, server = server)
